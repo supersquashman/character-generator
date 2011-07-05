d79e04217866cd1b4406888e10a51c34a8e73172
supersquashman@gmail.com <supersquashman@gmail.com@40b06a08-d936-a83c-591e-001f09d55f73>
2011-07-05 16:59:23 +0000

    <not sure about this yet>

---
+++ branches/D&D/Resources/Models/ClassModel.rb
@@ -0,0 +1,38 @@
+class ClassModel
+	attr_accessor :good_save,:bad_save,:good_bab,:mid_bab,:bad_bab
+	@@hd = "1d4"
+	def self.Class_skills
+		Class_skills
+	end
+	def self.Skill_mul
+		2
+	end
+	
+	def self.apply(character)
+		character.classes.push(self.new)
+	end
+	def self.available?(character)
+		return true
+	end
+	
+	def self.class_level(char)
+		cl=0
+		
+		char.classes.each do  |cla| 
+			if cla!=self.to_s
+			cl+=1
+			end
+		end
+		
+		return cl
+	end
+	def self.skill_ranks (char)
+		return ( (self.Skill_mul+char.mods["int"]) * (char.classes.length>1? 1:4))
+	end
+	
+	Good_save = [2,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1]
+	Bad_save = [0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0,1,0,0]
+	Good_bab = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1]
+	Mid_bab =  [0,1,1,1,0,1,1,1,0,1,1,1,0,1,1,1,0,1,1,1]
+	Bad_bab =  [0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1]
+end
