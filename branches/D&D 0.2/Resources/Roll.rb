942d4ca81eb03a0ace6e1ad59e817efc59f3c158
muddawg87@gmail.com <muddawg87@gmail.com@40b06a08-d936-a83c-591e-001f09d55f73>
2011-07-07 20:36:32 +0000

    [package title] = FishTornado D&D Character Generator

--- branches/D&D 0.2/Resources/Roll.rb
+++ branches/D&D 0.2/Resources/Roll.rb
@@ -1,20 +1,20 @@
 # Roll - manager for string based dice rolls
 # Copyright (C) 2011  Cody Garrett, Josh Murphy, and Matt Ingram
 
-# This file is part of [package title].
+# This file is part of FishTornado D&D Character Generator.
 
-# [package title] is free software: you can redistribute it and/or modify
+# FishTornado D&D Character Generator is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 
-# [package title] is distributed in the hope that it will be useful,
+# FishTornado D&D Character Generator is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
 
 # You should have received a copy of the GNU General Public License
-# along with [package title].  If not, see <http://www.gnu.org/licenses/>.
+# along with FishTornado D&D Character Generator.  If not, see <http://www.gnu.org/licenses/>.
 
 
 #--== Roll =============================================================================#
