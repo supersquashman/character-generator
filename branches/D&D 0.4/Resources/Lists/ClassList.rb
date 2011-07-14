b1fb8de1d1f5f411558d4eb1d1967a6a3748409a
supersquashman@gmail.com <supersquashman@gmail.com@40b06a08-d936-a83c-591e-001f09d55f73>
2011-07-14 18:05:31 +0000

    added header

--- branches/D&D 0.4/Resources/Lists/ClassList.rb
+++ branches/D&D 0.4/Resources/Lists/ClassList.rb
@@ -1,14 +1,36 @@
-require_relative "../Models/ClassModel"
+# ClassList - Model of D&D skills and container for related information
+# Copyright (C) 2011  Cody Garrett, Josh Murphy, and Matt Ingram
 
+# This file is part of FishTornado D&D Character Generator.
+
+# FishTornado D&D Character Generator is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# FishTornado D&D Character Generator is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with FishTornado D&D Character Generator.  If not, see <http://www.gnu.org/licenses/>.
+
+require_relative "../Models/ClassModel"
+#--== ClassList ========================================================================#
+#++
+# Members :
+
+# Methods : 
 class ClassList
-	@@list=Hash.new
-	def list
-		return @@list
-	end
-	def self.push(classs)
-		@@list[classs.to_s] = classs
-	end
-	def self.list
-		return @@list
-	end
+        @@list=Hash.new
+        def list
+                return @@list
+        end
+        def self.push(classs)
+                @@list[classs.to_s] = classs
+        end
+        def self.list
+                return @@list
+        end
 end
