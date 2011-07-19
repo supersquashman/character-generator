ab8cd10ab68d7abd49e3ba1f4771dccf3595a66d
muddawg87@gmail.com <muddawg87@gmail.com@40b06a08-d936-a83c-591e-001f09d55f73>
2011-07-19 21:32:15 +0000

    Debugging....

--- branches/D&D 0.4/Resources/Character.rb
+++ branches/D&D 0.4/Resources/Character.rb
@@ -139,6 +139,7 @@ class Character
 		found = false
 		self.abilities.each do |ability|
 			if ability.to_s.include? name.scan(/[^0-9]/)[0]
+        print '"' + name.scan(/[^0-9]/)[0] + '"'
 				inc_name=ability.sub(/\d{1,}/) {|n| n.to_i+number}
 				self.abilities[self.abilities.index(ability)] = inc_name
 				found = true
