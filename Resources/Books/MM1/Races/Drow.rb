a776db63c3e586ff51ac668743f70d2add061b89
supersquashman <supersquashman@gmail.com>
2011-07-22 21:44:25 +0000

    -Added Drow -Dex wasn't being applied to AC -Few other minor things.

--- Resources/Books/MM1/Races/Drow.rb
+++ Resources/Books/MM1/Races/Drow.rb
@@ -1,4 +1,4 @@
-# Elf - Model of D&D race and container for related information
+# Drow - Model of D&D race and container for related information
 # Copyright (C) 2011  Cody Garrett, Josh Murphy, and Matt Ingram
 
 # This file is part of FishTornado D&D Character Generator.
@@ -16,9 +16,9 @@
 # You should have received a copy of the GNU General Public License
 # along with FishTornado D&D Character Generator.  If not, see <http://www.gnu.org/licenses/>.
 
-#--== Elf ============================================================================#
+#--== Drow ============================================================================#
 #++
-class Elf< RaceModel
+class Drow< RaceModel
 
 #-- initialize(character) --------------------------------------------------------------#
 #++
@@ -29,7 +29,8 @@ class Elf< RaceModel
 		@age_roll = Roll.new("10d6+110")
 		@favored_classes=["Wizard"]
 		character.languages.learn_lang("Elven")
-		@bonus_languages = ["Draconic", "Gnoll", "Gnome", "Goblin", "Orc", "Sylvan"]
+		character.languages.learn_lang("Undercommon")
+		@bonus_languages = ["Abyssal", "Aquan", "Draconic", "Drow Sign Language", "Gnome", "Goblin"]
 	end
   
 #-- apply_level ------------------------------------------------------------------------#
@@ -40,21 +41,30 @@ class Elf< RaceModel
 			if (character.sex == "Male") #male
 				character.height = Roll.new("2d6+53")
 				character.weight = (85 +((character.height - 53) * Roll.new("1d6").to_i)) #extra weight is determined by multiplying the weight multiplier by the extra height
+				@favored_class = ["Wizard"]
 			end
 			if (character.sex == "Female") #female
 				character.height = Roll.new("2d6+53")
 				character.weight = (80 +((character.height - 53) * Roll.new("1d6").to_i)) #extra weight is determined by multiplying the weight multiplier by the extra height
+				@favored_class = ["Cleric"]
 			end
-			character.stats["dex"] += 2
-			character.stats["con"] -= 2                     
-			character.skill_list.assign_circ("Listen", "Racial (Elf)", 2)
-			character.skill_list.assign_circ("Spot", "Racial (Elf)", 2)
-			character.skill_list.assign_circ("Search", "Racial (Elf)", 2)
-			character.add_ability("Low-Light Vision")
-			character.weapon_proficiencies |= ["Shortbow", "Composite Shortbow", "Longbow", "Composite Longbow", "Rapier", "Longsword"]
+			character.ECL += 2
+			character.stats["int"] += 2
+			character.stats["cha"] += 2                     
+			character.skill_list.assign_circ("Listen", "Racial (Drow)", 2)
+			character.skill_list.assign_circ("Spot", "Racial (Drow)", 2)
+			character.skill_list.assign_circ("Search", "Racial (Drow)", 2)
+			character.add_ability("Darkvision (120)")
+			character.weapon_proficiencies |= ["Hand Crossbow", "Rapier", "Short Sword"]
 			character.add_ability("Immune to magic sleep effects")
+			character.add_ability("+2 Will Saves vs Spells/Spell-like-abilities")
 			character.add_ability("+2 Saves vs Enchantment Spells/Effects")
+			character.add_ability("Spell-like ability:  Darkness (1/day)")
+			character.add_ability("Spell-like ability:  Dancing Lights (1/day)")
+			character.add_ability("Spell-like ability:  Faerie Fire (1/day)")
+			character.add_ability("Light Blindness")
 		end
+		character.spell_resist = character.level + 11
 	end
 end
-RaceList.push(Elf)
+RaceList.push(Drow)
