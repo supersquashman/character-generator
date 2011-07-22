169a3dd4e84788eecbcb965c4352bedb1a8c8bfb
t0rm3n7@gmail.com <t0rm3n7@gmail.com@40b06a08-d936-a83c-591e-001f09d55f73>
2011-07-22 01:16:32 +0000

    Please go over this. Thanks :)

---
+++ branches/D&D 0.4/Resources/Books/PHB/Races/Elf.rb
@@ -0,0 +1,55 @@
+# Elf - Model of D&D race and container for related information
+# Copyright (C) 2011  Cody Garrett, Josh Murphy, and Matt Ingram
+
+# This file is part of FishTornado D&D Character Generator.
+
+# FishTornado D&D Character Generator is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# FishTornado D&D Character Generator is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with FishTornado D&D Character Generator.  If not, see <http://www.gnu.org/licenses/>.
+
+#--== Elf ============================================================================#
+#++
+class Elf< RaceModel
+
+#-- initialize(character) --------------------------------------------------------------#
+#++
+	def initialize(character)
+		super(character)
+		@size = "Medium"
+		@speed = 30
+		@age_roll = Roll.new("10d6+110")
+		@favored_classes=["Wizard"]
+		character.languages.learn_lang("Elven")
+		@bonus_languages = ["Draconic", "Gnoll", "Gnome", "Goblin", "Orc", "Sylvan"]
+	end
+  
+#-- apply_level ------------------------------------------------------------------------#
+#++
+	def apply_level
+		super
+		if(character.level <= 1)
+			character.stats["dex"] += 2
+			character.stats["con"] -= 2                     
+			character.skill_list.assign_circ("Listen", "Racial (Elf)", 2)
+			character.skill_list.assign_circ("Spot", "Racial (Elf)", 2)
+			character.skill_list.assign_circ("Search", "Racial (Elf)", 2)
+			character.add_ability("Low-Light Vision")
+			character.add_ability("Weapon Proficiency (Longsword)")
+			character.add_ability("Weapon Proficiency (Rapier)")
+			character.add_ability("Weapon Proficiency (Longbow)")
+			character.add_ability("Weapon Proficiency (Shortbow)")
+			character.add_ability("Immune to magic sleep effects")
+			character.add_ability("+2 Saves vs Enchantment Spells/Effects")
+		end
+	end
+end
+RaceList.push(Elf)
