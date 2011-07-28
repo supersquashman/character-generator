42e80ced3c8cf6dda979abf2ff02d0bdc4651cf3
supersquashman@gmail.com <supersquashman@gmail.com@40b06a08-d936-a83c-591e-001f09d55f73>
2011-07-28 20:11:57 +0000

    -finished monk class
    -added most of combat expertise tree
        -still missing whirlwind attack

---
+++ branches/D&D 0.4/Resources/Books/PHB/Feats/combat_expertise_feats.rb
@@ -0,0 +1,90 @@
+class CombatExpertise < FeatModel
+	@@bonus_classes = ["Fighter"]
+	
+	def initialize 
+		super
+		@title = "Combat Expertise"
+		@description = "When you use the attack action or the full attack action in melee, you can take a penalty of as much as -5 on your attack roll and add 
+						the same number (+5 or less) as a dodge bonus to your Armor Class. This number may not exceed your base attack bonus. The changes to attack 
+						rolls and Armor Class last until your next action."
+		@page = "PHB??"
+		@link = ""
+	end
+	
+	def self.available?(char)
+		return !feat_taken(char) && char.stats["int"] >= 13
+	end
+	
+	def self.is_bonus_feat?(class_type)
+		return @@bonus_classes.include?(class_type)
+	end
+end
+
+class ImprovedDisarm < FeatModel
+	@@bonus_classes = ["Fighter"]
+	
+	def initialize 
+		super
+		@title = "Combat Expertise"
+		@description = "You do not provoke an attack of opportunity when you attempt to disarm an opponent, nor does the opponent have a chance to 
+						disarm you. You also gain a +4 bonus on the opposed attack roll you make to disarm your opponent."
+		@page = "PHB??"
+		@link = ""
+	end
+	
+	def self.available?(char)
+		return !feat_taken(char) && char.stats["int"] >= 13 && CombatExpertise.feat_taken(char)
+	end
+	
+	def self.is_bonus_feat?(class_type)
+		return @@bonus_classes.include?(class_type)
+	end
+end
+
+class ImprovedFeint < FeatModel
+	@@bonus_classes = ["Fighter"]
+	
+	def initialize 
+		super
+		@title = "Combat Expertise"
+		@description = "You can make a Bluff check to feint in combat as a move action."
+		@page = "PHB??"
+		@link = ""
+	end
+	
+	def self.available?(char)
+		return !feat_taken(char) && char.stats["int"] >= 13 && CombatExpertise.feat_taken(char)
+	end
+	
+	def self.is_bonus_feat?(class_type)
+		return @@bonus_classes.include?(class_type)
+	end
+end
+
+class ImprovedTrip < FeatModel
+	@@bonus_classes = ["Fighter"]
+	
+	def initialize 
+		super
+		@title = "Combat Expertise"
+		@description = "You do not provoke an attack of opportunity when you attempt to trip an opponent while you are unarmed. You also gain a +4 bonus on your 
+						Strength check to trip your opponent.
+
+			If you trip an opponent in melee combat, you immediately get a melee attack against that opponent as if you hadn’t used your attack for the trip attempt."
+		@page = "PHB??"
+		@link = ""
+	end
+	
+	def self.available?(char)
+		return !feat_taken(char) && char.stats["int"] >= 13 && CombatExpertise.feat_taken(char)
+	end
+	
+	def self.is_bonus_feat?(class_type)
+		return @@bonus_classes.include?(class_type)
+	end
+end
+
+FeatList.push(CombatExpertise)
+FeatList.push(ImprovedDisarm)
+FeatList.push(ImprovedFeint)
+FeatList.push(ImprovedTrip)
