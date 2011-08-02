12fb1b23ae2f61e965d7e717a2c28da9b61efc7f
t0rm3n7 <t0rm3n7@gmail.com>
2011-08-02 05:32:55 +0000

    needs page numbers, but otherwise done.

--- Resources/Books/PHB/Magic/Spells/DruidSpells.rb
+++ Resources/Books/PHB/Magic/Spells/DruidSpells.rb
@@ -16,4 +16,200 @@
 # You should have received a copy of the GNU General Public License
 # along with FishTornado D&D Character Generator.  If not, see <http://www.gnu.org/licenses/>.
 
-SpellList.load_spells("0", "Druid", [])
+SpellList.load_spells("0", "Druid", [
+		["Create Water", "", "", "Creates 2gal/level of pure water"],
+		["Cure Minor Wounds", "", "", "Cures 1 point of dmg"],
+		["Detect Magic", "", "", "Detect spells and magic items within 60 ft."],
+		["Detect Poison", "", "", "Detects poison in one creature or object"],
+		["Flare", "", "", "Dazzles one creature (-1 penalty on attack rolls)"],
+		["Guidance", "", "", "+1 on one attack roll, saving throw, or skill check"],
+		["Know Direction", "", "", "You discern North"],
+		["Light", "", "", "Object shines like a torch"],
+		["Mending", "", "", "Makes minor repairs on an object"],
+		["Purify Food and Drink", "", "", "Purifies 1 cu. ft/level of food or water"],
+		["Read Magic", "", "", "Read scrolls and scrollbooks"],
+		["Resistance", "", "", "Subject gains +1 bonus on saving throws"],
+		["Virtue", "", "", "Subject gains 1 temporary hp"]
+		])
+		
+SpellList.load_spells("1", "Druid", [
+		["Calm Animals", "", "", "Calms 2d4+level HD of animals"],
+		["Charm Animal", "", "", "Makes one animal your friend"],
+		["Cure Light Wounds", "", "", "Cures 1d8 damage +1/level (max +5)"],
+		["Detect Animals or Plants", "", "", "Detects kinds of animals or plants"],
+		["Detect Snares and Pits", "", "", "Reveals natural or primitive traps"],
+		["Endure Elements", "", "", "Exist comfortably in hot and cold enviroments"],
+		["Entangle", "", "", "Plants entangle everyone in 40ft. radius"],
+		["Faerie Fire", "", "", "Outlines subject with light, cancelling blur, concealment, and the like"],
+		["Goodberry", "", "", "2d4 berries each cure 1 hp (max 8hp/24 hours)"],
+		["Hide from Animals", "", "", "Animals can't percieveone subject/level"],
+		["Jump", "", "", "Subject gets bonus on jump checks"],
+		["Longstrider", "", "", "Your speed increases by 10ft"],
+		["Magic Fang", "", "", "One natural weapon of subject creature gets +1 on attack and damage rolls"],
+		["Magic Stone", "", "", "Three stones gain +1 on attack rolls, deal 1d6+1 damage"],
+		["Obscuring Mist", "", "", "Fog surrounds you."],
+		["Pass without Trace", "", "", "One subject/level leaves no tracks"],
+		["Produce Flame", "", "", "1d6 damage +1/level; touch or thrown"],
+		["Shillelagh", "", "", "Cudgel or quarterstaff becomes +1 weapon and deals damage as if two sizes larger"],
+		["Speak with Animals", "", "", "You can communicate with animals"],
+		["Summon Nature's ally I", "", "", "Calls a creature to fight"]
+		])
+		
+SpellList.load_spells("2", "Druid", [
+		["Animal Messenger", "", "", "Sends a tiny animal to a specific place"],
+		["Animal Trance", "", "", "Fascinates 2d6 HD of animals"],
+		["Barkskin", "", "", "Grants +2 or higher enhancement to natural armor"],
+		["Bear's Endurance", "", "", "Subject gains +4 to Con for 1min/level"],
+		["Bull's Strength", "", "", "Subject gains +4 to Str for 1min/level"],
+		["Cat's Grace", "", "", "Subject gains +4 to Dex for 1min/level"],
+		["Chill Metal", "", "", "Cold metal damages those who touch it"],
+		["Delay Poison", "", "", "Stops poison from harming subject for 1hr/level"],
+		["Fire Trap", "", "", "Opened object deals 1d4+1/level damage"],
+		["Flame Blade", "", "", "Touch attack deals 1d8+1/two levels damage"],
+		["Flaming Sphere", "", "", "Creates rolling ball of fire, 2d6 damage, lasts 1 round/level"],
+		["Fog Cloud", "", "", "Fog obscures vision"],
+		["Gust of Wind", "", "", "Blows away or knocks down smaller creatures"],
+		["Heat Metal", "", "", "Make metal so hot it damages those who touch it"],
+		["Hold Animal", "", "", "Paralyzes one animal for 1 round/level"],
+		["Owl's Wisdom", "", "", "Subject gains +4 to Wis for 1min/level"],
+		["Reduce Animal", "", "", "Shrinks one willing animal"],
+		["Resist Energy", "", "", "Ignores 10 or more points of damage/attack from specified energy type"],
+		["Restoration, Lesser", "", "", "Dispels magical ability penalty or repairs 1d4 ability damage"],
+		["Soften Earth and Stone", "", "", "Turns stone to clay, or dirt to sand or mud"],
+		["Spider Climb", "", "", "Grants ability to walk on walls and ceilings"],
+		["Summon Nature's Ally II", "", "", "Calls creature to fight"],
+		["Summon Swarm", "", "", "Summons swarm of bats, rats, or spiders"],
+		["Tree Shape", "", "", "You look exactly like a tree for 1hr/level"],
+		["Warp Wood", "", "", "Bends wood (shaft, handle, door, plank)"],
+		["Wood Shape", "", "", "Rearranges wooden objects to suit you"]
+		])
+		
+SpellList.load_spells("3", "Druid", [
+		["Call Lightning", "", "", "Calls dwon lightning bolts (3d6 per bolt) from the sky"],
+		["Contagion", "", "", "Infects subject with chosen disease"],
+		["Cure Moderate Wounds", "", "", "cures 2d8 +1/level (max +10)"],
+		["Daylight", "", "", "60ft radius of bright light"],
+		["Diminish Plants", "", "", "Reduces size or blights growth of normal plants"],
+		["Magic Fang, Greater", "", "", "One natural weapon of subject creature gets +1/four level on attack and damage rolls"],
+		["Meld into Stone", "", "", "You and your gear merge with stone"],
+		["Neutralize Poison", "", "", "Immunizes subject against potion, detoxifies venom in or on subject"],
+		["Plant Growth", "", "", "Grows vegetation, improves crops"],
+		["Poison", "", "", "Touch deals 1d10 Con damage, repeats in 1 min"],
+		["Protection from Energy", "", "", "Absorb 12 points/level of damage from one kind of energy"],
+		["Quench", "", "", "Extinguishes nonmagical fires or one magic item"],
+		["Remove Disease", "", "", "Cures all diseases affecting subject"],
+		["Sleet Storm", "", "", "Hampers vision and movement"],
+		["Snare", "", "", "Creates a magic booby trap"],
+		["Speak with Plants", "", "", "You can talk to normal plants and plant creatures"],
+		["Spike Growth", "", "", "Creatures in area tak 1d4 damage, may be slowed"],
+		["Stone Shape", "", "", "Sculpts stone into any shape"],
+		["Dominate Animal", "", "", "Subject animal obeys silent mental commands"],
+		["Summon Nature's Ally III", "", "", "Calls creature to fight"],
+		["Water Breathing", "", "", "Subjects can breathe underwater"],
+		["Wind Wall", "", "", "Deflects arrows, smaller creatures, and gases"]
+		])
+SpellList.load_spells("4", "Druid", [
+		["Air Walk", "", "", "Subject treads on air as if on solid ground(climb at 45-degree angle)"],
+		["Antiplant Shell", "", "", "Keeps animated plants at bay"],
+		["Blight", "", "", "Withers one plant or deals 1d6/level damage to plant creature"],
+		["Command Plants", "", "", "Sway the actions of one or more plant creatures"],
+		["Control Water", "", "", "Raises or lowers bodies of water"],
+		["Cure Serious Wounds", "", "", "Cures 3d8 damage +1/level (max +15)"],
+		["Dispel Magic", "", "", "Cancels spells and magical effects"],
+		["Flame Strike", "", "", "Smite foes with divine fire (1d6/level damage)"],
+		["Freedom of Movement", "", "", "Subject moves normally despite impediments"],
+		["Giant Vermin", "", "", "Turns centipedes, scorpions, or spiders into giant vermin"],
+		["Ice Storm", "", "", "Hails deals 5d6 damage in cylinder 40ft across"],
+		["Reincarnate", "", "", "Brings dead subject back in a random body"],
+		["Repel Vermin", "", "", "Inscets, spiders, and other vermin stay 10ft away"],
+		["Rusting Grasp", "", "", "Your touch corrodes iron and alloys"],
+		["Scrying", "", "", "Spies on subject from a distance"],
+		["Spike Stones", "", "", "Creatures in area take 1d8 damage, may be slowed"],
+		["Summon Nature's Ally IV", "", "", "Calls creature to fight"]
+		])
+		
+SpellList.load_spells("5", "Druid", [
+		["Animal Growth", "", "", "One animal/two levels doubles in size"],
+		["Atonement", "", "", "Removes burden of misdeeds from subject"],
+		["Awaken", "", "", "Animal or tree gains humal intellect"],
+		["Baleful Polymorph", "", "", "Transforms subject into harmless animal"],
+		["Call Lightning Storm", "", "", "As call lightning, but 5d6 damage per bolt"],
+		["Commune with Nature", "", "", "Learn about terrain for 1 mile/level"],
+		["Control Winds", "", "", "Change wind direction and speed"],
+		["Cure Critical Wounds", "", "", "Cures 4d8 +1/level (max +20)"],
+		["Death Ward", "", "", "Grants immunity to all death spells and negative energy effects"],
+		["Hallow", "", "", "Designates location as holy"],
+		["Insect Plague", "", "", "Locust swarms attack creatures"],
+		["Stoneskin", "", "", "Ignore 10 points of damage per attack"],
+		["Summon Nature's Ally V", "", "", "Calls creature to fight"],
+		["Transmute Mud to Rock", "", "", "Transforms two 10ft cubes per level"],
+		["Transmute Rock to Mud", "", "", "Transforms two 10ft cubes per level"],
+		["Tree Stride", "", "", "Step from one tree to another far away"],
+		["Unhallow", "", "", "Designates location as unholy"],
+		["Wall of Fire", "", "", "Deals 2d4 fire damage out to 10ft and 1d4 out to 20ft. Passing through wall deals 2d6 damage +1/level"],
+		["Wall of Thorns", "", "", "Thorns damage anyone who tries to pass"]
+		])
+		
+SpellList.load_spells("6", "Druid", [
+		["Antilife Shell", "", "", "10ft radius field hedges out living creatures"],
+		["Bear's Endurance, Mass", "", "", "As Bear's Endurance, affects one subject/level"],
+		["Bull's Strength, Mass", "", "", "As Bull's Strength, affects one subject/level"],
+		["Cat's Grace, Mass", "", "", "As Cat's Grace, affects one subject/level"],
+		["Cure Light Wounds, Mass", "", "", "Cures 1d8 damage +1/level for many creatures"],
+		["Dispel Magic, Greater", "", "", "As Dispel Magic, but +20 on check"],
+		["Find the Path", "", "", "Shows most direct way to a location"],
+		["Fire Seeds", "", "", "Acorns and berries become grenades and bombs"],
+		["Ironwood", "", "", "Magic wood is as strong as steel"],
+		["Liveoak", "", "", "Oak becomes treant guardian"],
+		["Move Earth", "", "", "Digs trenches and builds hills"],
+		["Owl's Wisdom, Mass", "", "", "As Owl's Wisdom, affects one subject/level"],
+		["Repel Wood", "", "", "Pushes away wooden objects"],
+		["Spellstaff", "", "", "Stores one spell in wooden quarterstaff"],
+		["Stone Tell", "", "", "Talk to natural or worked stone"],
+		["Summon Nature's Ally VI", "", "", "Calls creature to fight"],
+		["Transport via Plants", "", "", "Move instantly from one plant to another of the same kind"],
+		["Wall of Stone", "", "", "Creates a stone wall that can be shaped"]
+		])
+		
+SpellList.load_spells("7", "Druid", [
+		["Animate Plants", "", "", "One or more plants animate and fight for you"],
+		["Changestaff", "", "", "Your staff becomes a treant on command"],
+		["Control Weather", "", "", "Changes weather in local area"],
+		["Creeping Doom", "", "", "Swarms of centipedes attack at your command"],
+		["Cure Moderate Wounds, Mass", "", "", "Cures 2d8 +1/level for many creatures"],
+		["Fire Storm", "", "", "Deals 1d6/level fire damage"],
+		["Heal", "", "", "Cure 10 points/level of damage, all diseases and mental conditions"],
+		["Scrying, Greater", "", "", "As Scrying, but faster and longer"],
+		["Summon Nature's Ally VII", "", "", "Calls creature to fight"],
+		["Sunbeam", "", "", "Beam blinds and deals 4d6 damage"],
+		["Transmute Metal to Wood", "", "", "Metal within 40ft becomes wood"],
+		["True Seeing", "", "", "Lets you see all things as they really are"],
+		["Wind Walk", "", "", "You and your allies turn vaporous and travel fast"]
+		])
+		
+SpellList.load_spells("8", "Druid", [
+		["Animal Shapes", "", "", "One ally/level polymorphs into chosen animals"],
+		["Control Plants", "", "", "control actions of one or more plant creatures"],
+		["Cure Serious Wounds, Mass", "", "", "Cures 3d8 damage +1/level for many creatures"],
+		["Earthquake", "", "", "Intense tremor shakes 80ft radius"],
+		["Finger of Death", "", "", "Kills one subject"],
+		["Repel Metal or Stone", "", "", "Pushes away metal and stone"],
+		["Reverse Gravity", "", "", "Object and creatures fall upward"],
+		["Summon Nature's Ally VIII", "", "", "Calls creature to fight"],
+		["Sunburst", "", "", "Blinds all within 10ft, deals 6d6 damage"],
+		["Whirlwind", "", "", "Cyclone deals damage and can pick up creatures"],
+		["Word of Recall", "", "", "Teleport you back to designated place"]
+		])
+		
+SpellList.load_spells("9", "Druid", [
+		["Antipathy", "", "", "Object or location affected by spell repels certain creatures"],
+		["Cure Critical Wounds, Mass", "", "", "Cures 4d8 damage +1/level for many creatures"],
+		["Elemental Swarm", "", "", "Summons multiple elementals"],
+		["Foresight", "", "", "\"Sixth Sense\" warns of impending danger"],
+		["Regenerate", "", "", "Subject's severed limbs grow back, cures 4d8 damage +1/level (max +35)"],
+		["Shambler", "", "", "Summons 1d4+2 shamblimg mounds to fight for you"],
+		["Shapechange", "", "", "Transforms you into any creature, and change forms once per round"],
+		["Storm of Vengeance", "", "", "Storm rains acid, lightning, and hail"],
+		["Summon Nature's Ally", "", "", "Calls creature to fight"],
+		["Sympathy", "", "", "Object or location attracts certain creatures"]
+		])
