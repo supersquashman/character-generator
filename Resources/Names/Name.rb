66c7ab4ae1c35c9fc2064c897b5c30d300b3e5f4
supersquashman <supersquashman@gmail.com>
2011-08-04 16:23:50 +0000

    Sorta working...

--- Resources/Names/Name.rb
+++ Resources/Names/Name.rb
@@ -1,17 +1,17 @@
 class Name
 attr_accessor :name, :pre, :post
 	@@tokens = Hash.new(Hash.new(Hash.new([])))
-  @token_categories = ["General"]
+	@token_categories = ["General"]
 	@alignment=""
   
 	def self.load(relative_path="Names")
 		Dir.glob(File.join(".", relative_path, "*/*.txt")).each do |file|
-    #Dir.glob(File.join(".", "*/*.txt")).each do |file|
+		#Dir.glob(File.join(".", "*/*.txt")).each do |file|
 			File.open(file, "r") do |file|
-				while(line = file.gets)
+				while(line = file.gets) 
 					hash2 = File.basename(file.path, ".txt")
-          hash1 = File.dirname(file.path).match(/[^\/]*$/).to_s
-          @@tokens["[" + hash1 + "]"] = Hash.new([]) if @@tokens["[" + hash1 + "]"].length < 1
+					hash1 = File.dirname(file.path).match(/[^\/]*$/).to_s
+					@@tokens["[" + hash1 + "]"] = Hash.new([]) if @@tokens["[" + hash1 + "]"].length < 1
 					@@tokens["[" + hash1 + "]"]["[" + hash2 + "]"] += [line.chomp] if line.chomp!=""
 				end
 			end
@@ -21,34 +21,34 @@ attr_accessor :name, :pre, :post
 	def initialize(character)
 		@result = ""
 		@alignment= character.alignment
-    @gender = rand(20)!=0 ? character.sex : ["Male","Female"][(rand(2))] #gender confusion?
-    @race = character.race.to_s
-    @name_token_categories = ["[General]", "[" + @race + "]"]
-    @name = ""
-    @pre = ""
-    @post = ""
+		@gender = rand(20)!=0 ? character.sex : ["Male","Female"][(rand(2))] #gender confusion?
+		@race = character.race.to_s
+		@name_token_categories = ["[General]", "[" + @race + "]"]
+		@name = ""
+		@pre = ""
+		@post = ""
 		generate
 	end
 
 	def generate(alignment=@alignment)
-    begin 
-    choice=@name_token_categories[rand(@name_token_categories.length)] 
-    end while(@@tokens[choice].length < 1)
-    @name = pick_line(choice,"[rules]").split(" ").each{|word| word.capitalize!}.join(" ")
-    @pre = pick_line(choice,"[pretitle]").split(" ").each{|word| word.capitalize!}.join(" ")
-    @post = pick_line("[Title]","[title]").split(" ").each{|word| word.capitalize!}.join(" ")
+		begin 
+			choice=@name_token_categories[rand(@name_token_categories.length)] 
+		end while(@@tokens[choice].length > 1)
+		@name = pick_line(choice,"[rules]").split(" ").each{|word| word.capitalize!}.join(" ")
+		@pre = pick_line(choice,"[pretitle]").split(" ").each{|word| word.capitalize!}.join(" ")
+		@post = pick_line("[Title]","[title]").split(" ").each{|word| word.capitalize!}.join(" ")
 	end
 
 	def pick_line (hash="[Dwarf]", hash2="[rules]", alignment=@alignment, gender=@gender)
 		line = ""
 		if @@tokens[hash][hash2].length > 0
-      parts = []
+			parts = []
 			begin while ((line = @@tokens[hash][hash2][rand(@@tokens[hash][hash2].length)].dup) == ""); end
-        parts = line.partition("|")
-        line = parts[1]=="" ? parts[0] : parts[2]
-      end while (! qualified(parts[1]=="" ? "" : parts[0],alignment))
-      line.gsub!(/\[[^\]]+\]/){|new_hash| pick_line(hash, new_hash)}
-      #return line
+				parts = line.partition("|")
+				line = parts[1]=="" ? parts[0] : parts[2]
+			end while (! qualified(parts[1]=="" ? "" : parts[0],alignment))
+			line.gsub!(/\[[^\]]+\]/){|new_hash| pick_line(hash, new_hash)}
+			#return line
 		end
 		return line
 	end
