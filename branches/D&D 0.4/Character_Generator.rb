57dee2cf1cc7fb27cf7eb8843cb631eb288b76e0
supersquashman@gmail.com <supersquashman@gmail.com@40b06a08-d936-a83c-591e-001f09d55f73>
2011-08-04 16:23:50 +0000

    Sorta working...

--- branches/D&D 0.4/Character_Generator.rb
+++ branches/D&D 0.4/Character_Generator.rb
@@ -1,24 +1,32 @@
-require_relative "Resources/Character"
+require_relative "./Resources/Character"
+Dir.glob(File.join(".", "/Resources/Lists", "*.rb")).each do |file|
+   require file
+end
+Dir.glob(File.join(".", "/Resources/Models", "*.rb")).each do |file|
+   require file
+end
+require_relative "Resources/Backgrounds/Background"
+require_relative "Resources/Names/Name"
 
 class CharacterGenerator
 	@@character_list = []
 
 	def generate_specific_level_characters(char_level = 1, book_list = ["PHB"], char_count = 1)
-		book_list.each { |book| Dir.glob("./**/**/"+book+"/*Races/*.rb").each {|file| require file} }
-		book_list.each { |book| Dir.glob("./**/**/"+book+"/*Classes/*.rb").each {|file| require file} }
-		book_list.each { |book| Dir.glob("./**/**/"+book+"/*Feats/*.rb").each {|file| require file} }
-		book_list.each { |book| Dir.glob("./**/**/"+book+"/*Items/*.rb").each {|file| require file} }
+		book_list.each { |book| Dir.glob("./*Resources/*Books/"+book+"/*Races/*.rb").each {|file| require file} }
+		book_list.each { |book| Dir.glob("./*Resources/*Books/"+book+"/*Classes/*.rb").each {|file| require file} }
+		book_list.each { |book| Dir.glob("./*Resources/*Books/"+book+"/*Feats/*.rb").each {|file| require file} }
+		book_list.each { |book| Dir.glob("./*Resources/*Books/"+book+"/*Items/*.rb").each {|file| require file} }
 		
 		char_count.times do
 			@@character_list.push(Character.new(book_list))
 		end
-		@@character_list.times do |character|
+		@@character_list.each do |character|
 			character.max_classes = 1
 			char_level.times do
 				character.level_up
 			end
+			character.final_levelup_procs.each{|proc| proc.call character}
 		end
-		character.final_levelup_procs.each{|proc| proc.call character}
 	end
 	
 	def generate_level_range_characters(char_low_level =1, char_high_level = 20, book_list = ["PHB"], char_count = 1)
@@ -42,13 +50,13 @@ class CharacterGenerator
 		@@character_list.each do |character|
 			created = false
 			new_file_count = 0
-			file_name = "Characters/" + character.name + ".txt"
+			file_name = "Characters/" + character.name.to_s + ".txt"
 			while (!created)
-				if (File.exists(file_name))
+				if (File.exists?(file_name))
 					new_file_count += 1
-					file_name = "Characters/" + character.name + "(" + new_file_count.to_s + ").txt"
+					file_name = "Characters/" + character.name.to_s + "(" + new_file_count.to_s + ").txt"
 				else
-					char_file = File.new(file_name, w)
+					char_file = File.new(file_name, "w")
 					created = true
 				end
 			end
