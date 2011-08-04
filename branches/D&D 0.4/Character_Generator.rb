bcdb4acb095d4c7d26b7dc808ddb3515c645d1f5
supersquashman@gmail.com <supersquashman@gmail.com@40b06a08-d936-a83c-591e-001f09d55f73>
2011-08-04 16:56:01 +0000

    updated file writer

--- branches/D&D 0.4/Character_Generator.rb
+++ branches/D&D 0.4/Character_Generator.rb
@@ -57,9 +57,65 @@ class CharacterGenerator
 					file_name = "Characters/" + character.name.to_s + "(" + new_file_count.to_s + ").txt"
 				else
 					char_file = File.new(file_name, "w")
+					print_character(char_file)
+					char_file.close
 					created = true
 				end
 			end
 		end
+	end
+	
+	def print_character(file)
+		file.puts "Name:  " + panda.name.to_s
+		file.puts "Race:  " + template_race_list.join(" ").to_s + " " + panda.race.to_s
+		file.puts "Alignment: " + panda.alignment
+		file.puts "Age:  " + panda.age.to_s
+		file.puts "Gender:  " + panda.sex
+		file.puts "Height:  " + ((panda.height.to_i/12).floor).to_s + "'" + ((panda.height.to_i%12)).to_s + "\""
+		file.puts "Weight:  " + panda.weight.to_s + " lbs."
+		file.puts "Base Attack Bonus:  " + panda.BAB.to_s
+		file.puts "Base Land Speed:  " + panda.speed.to_s
+		file.puts "AC:  " + panda.ac_list.values.inject(0){|sum,item| sum.to_i + item.to_i}.to_s
+		(file.puts "Spell Resistance:  " + panda.spell_resist.to_s) if panda.spell_resist > 0
+		file.puts ""
+		["str","dex","con","int","wis","cha"].each do |stat|
+			if (panda.stat_mod[stat] >= 0)
+				file.puts stat + "	" + panda.stats[stat].to_s + " (+" + panda.stat_mod[stat].to_s + ")"
+			else
+				file.puts stat + "	" + panda.stats[stat].to_s + " (" + panda.stat_mod[stat].to_s + ")"
+			end
+		end
+		file.puts panda.skill_list
+		file.puts ""
+		file.puts panda.abilities.sort
+		#file.puts "\nFavored Classes:  "
+		#panda.race.favored_classes.each {|cls| file.puts cls}
+		file.puts ""
+		file.puts "Languages:"
+		file.puts panda.languages
+		file.puts "Classes:  "
+		temp_classes = []
+		panda.classes.each {|cls| temp_classes.push(cls.to_s)}
+		temp_classes.uniq.each do |cls|
+			#file.puts cls.to_s + "("+ panda.classes.collect {|val| val.class.to_s == cls}.size.to_s + ")"
+			file.puts cls.to_s + "(" + temp_classes.count(cls).to_s + ")"
+		end
+		file.puts ""
+		file.puts "Feats:"
+		sorted_feats = []
+		panda.feats.each do |feat|
+			sorted_feats += [feat.to_s]
+		end
+		file.puts sorted_feats.sort
+		if panda.spells.to_s
+			file.puts "\nSpells:"
+			file.puts panda.spells.to_s
+		end
+		file.puts "\nBackground:"
+		file.puts panda.history
+		file.puts ""
+		file.puts panda.primary_motivation
+		file.puts ""
+		file.puts panda.secondary_motivation
 	end
 end
