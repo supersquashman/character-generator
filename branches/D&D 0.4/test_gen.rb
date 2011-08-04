4d193f605245be9129fc97cba0a52b4a771025b6
supersquashman@gmail.com <supersquashman@gmail.com@40b06a08-d936-a83c-591e-001f09d55f73>
2011-08-04 17:36:54 +0000

    Tweaks

--- branches/D&D 0.4/test_gen.rb
+++ branches/D&D 0.4/test_gen.rb
@@ -1,5 +1,10 @@
 require_relative "Character_Generator"
 
+print "Number of characters to generate:  "
+number_of_characters = gets.chomp
+print "Character level:  "
+char_level = gets.chomp
+
 generator = CharacterGenerator.new
-generator.generate_specific_level_characters
+generator.generate_specific_level_characters(number_of_characters.to_i, char_level.to_i)
 generator.save_characters
