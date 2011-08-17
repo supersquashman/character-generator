d97e1f494d1544728e2656d632ff4a9e755e2d0c
supersquashman@gmail.com <supersquashman@gmail.com@40b06a08-d936-a83c-591e-001f09d55f73>
2011-08-17 18:02:10 +0000

    Storm Giants

--- branches/D&D 0.5/Resources/Books/MM1/Races/Storm_Giant.rb
+++ branches/D&D 0.5/Resources/Books/MM1/Races/Storm_Giant.rb
@@ -1,4 +1,4 @@
-# Cloud Giant - Model of D&D race and container for related information
+# Storm Giant - Model of D&D race and container for related information
 # Copyright (C) 2011  Cody Garrett, Josh Murphy, and Matt Ingram
 
 # This file is part of FishTornado D&D Character Generator.
@@ -16,9 +16,9 @@
 # You should have received a copy of the GNU General Public License
 # along with FishTornado D&D Character Generator.  If not, see <http://www.gnu.org/licenses/>.
 
-#--== Cloud Giant ============================================================================#
+#--== Storm Giant ============================================================================#
 #++
-class CloudGiant < RaceModel
+class StormGiant < RaceModel
 
 #-- initialize(character) --------------------------------------------------------------#
 #++
@@ -26,11 +26,11 @@ class CloudGiant < RaceModel
 		super(character)
 		@size = "Huge"
 		@speed = 50
-		@age_roll = Roll.new("10d10+75")
+		@age_roll = Roll.new("17d10+100")
 		@favored_classes=["Cleric", "Sorcerer"]
 		character.languages.learn_lang("Giant")
 		@bonus_languages = ["Common", "Draconic", "Elven", "Goblin", "Orc"]
-		@racial_HD = "17d8"
+		@racial_HD = "19d8"
 	end
   
 #-- apply_level ------------------------------------------------------------------------#
@@ -39,41 +39,44 @@ class CloudGiant < RaceModel
 		super
 		if(character.level <= 1)
 			if (character.sex == "Male") #male
-				character.height = Roll.new("4d12+192")
-				character.weight = (character.height.to_i * 23.148) #extra weight is determined by multiplying the weight multiplier by the extra height
+				character.height = Roll.new("5d12+216")
+				character.weight = (character.height.to_i * 47.619) #extra weight is determined by multiplying the weight multiplier by the extra height
 			end
 			if (character.sex == "Female") #female
-				character.height = Roll.new("4d12+190")
-				character.weight = (character.height.to_i * 23.138) #extra weight is determined by multiplying the weight multiplier by the extra height
+				character.height = Roll.new("5d12+210")
+				character.weight = (character.height.to_i * 47.619) #extra weight is determined by multiplying the weight multiplier by the extra height
 			end
-			character.ECL += 6
-			character.HD += 17
-			character.BAB += 12
+			character.ECL += 8
+			character.HD += 19
+			character.BAB += 14
 			character.HP += Roll.new(@racial_HD).to_i
-			character.stats["str"] += 24
-			character.stats["cha"] += 2
+			character.stats["str"] += 28
+			character.stats["cha"] += 5
 			character.stats["con"] += 12
-			character.stats["dex"] += 3
-			character.stats["int"] += 2
-			character.stats["wis"] += 6
-			character.fort_save["racial"] = 16
-			character.ref_save["racial"] = 6
-			character.will_save["racial"] = 10
+			character.stats["dex"] += 4
+			character.stats["int"] += 6
+			character.stats["wis"] += 10
+			character.fort_save["racial"] = 17
+			character.ref_save["racial"] = 8
+			character.will_save["racial"] = 13
 			character.ac_list["natural"] += 12
-			FeatList.roll_feats(character, 5)
+			FeatList.roll_feats(character, 6)
 			num_skills = (character.stat_mod["int"] + 2) * 15
-			character.skill_list.class_skills = ["Climb","Jump","Intimidate","Spot", "Sense Motive", "Diplomacy"]
+			character.skill_list.class_skills = ["Climb", "Concentration", "Jump", "Intimidate", "Listen", "Spot", "Sense Motive", "Diplomacy"]
 			character.skill_list.roll_skills(num_skills)
+			character.skill_list.assign_misc("Swim", 8)
 			#Craft (any one) +6
 			character.add_ability("Low-Light Vision")
-			character.add_ability("Oversized Weapon")
+			character.add_ability("Freedom of Movement")
+			character.add_ability("Immunity to Electricity")
 			character.add_ability("Rock Catching")
-			character.add_ability("Scent")
-			character.add_ability("Levitate (3/day)")
-			character.add_ability("Obscuring Mist (3/day)")
-			character.add_ability("Fog Cloud (1/day)")
-			character.weapon_proficiencies |= ["Morningstar"]
+			character.add_ability("Water Breathing")
+			character.add_ability("Levitate (2/day)")
+			character.add_ability("Control Weather (2/day)")
+			character.add_ability("Call Lightning (1/day)")
+			character.add_ability("Chain Lightning (1/day)")
+			character.weapon_proficiencies |= ["Greatsword"]
 		end
 	end
 end
-RaceList.push(CloudGiant)
+RaceList.push(StormGiant)
