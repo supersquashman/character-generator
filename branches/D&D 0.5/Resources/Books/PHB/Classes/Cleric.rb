638107d63a5d9a6e67372546d09a80c8efaef2b1
supersquashman@gmail.com <supersquashman@gmail.com@40b06a08-d936-a83c-591e-001f09d55f73>
2011-08-22 21:50:15 +0000

    More require_relative >> require

--- branches/D&D 0.5/Resources/Books/PHB/Classes/Cleric.rb
+++ branches/D&D 0.5/Resources/Books/PHB/Classes/Cleric.rb
@@ -16,8 +16,8 @@
 # You should have received a copy of the GNU General Public License
 # along with FishTornado D&D Character Generator.  If not, see <http://www.gnu.org/licenses/>.
 
-require_relative "#{File.dirname(__FILE__)}/../Magic/Spells/ClericSpells"
-require_relative "#{File.dirname(__FILE__)}/../Magic/Spells/ClericDomains"
+require "#{File.dirname(__FILE__)}/../Magic/Spells/ClericSpells"
+require "#{File.dirname(__FILE__)}/../Magic/Spells/ClericDomains"
 
 #--== Cleric ===========================================================================#
 #++
--- branches/D&D 0.5/Resources/Books/PHB/Classes/Wizard.rb
+++ branches/D&D 0.5/Resources/Books/PHB/Classes/Wizard.rb
@@ -16,7 +16,7 @@
 # You should have received a copy of the GNU General Public License
 # along with FishTornado D&D Character Generator.  If not, see <http://www.gnu.org/licenses/>.
 
-require_relative "#{File.dirname(__FILE__)}/../Magic/Spells/WizardSpells"
+require "#{File.dirname(__FILE__)}/../Magic/Spells/WizardSpells"
 
 #--== Wizard ===========================================================================#
 #++
