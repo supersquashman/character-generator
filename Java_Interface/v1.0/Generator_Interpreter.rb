5ae0b3e671235ad39fe8812a1efc9149cfc822aa
supersquashman <supersquashman@40b06a08-d936-a83c-591e-001f09d55f73>
2011-12-01 09:07:47 +0000

    Derp >:(

--- Java_Interface/v1.0/Generator_Interpreter.rb
+++ Java_Interface/v1.0/Generator_Interpreter.rb
@@ -5,18 +5,22 @@ def generate(file_path = File.dirname(__FILE__), args = "")
 	argList[0] = argList[0].split(",")
 	argList[1] = argList[1].split(",")
 	test = File.new("test.txt","w")
-	test.puts(argList)
+	#test.puts(argList)
+	argList.each do |thingy|
+		test.puts argList.index(thingy).to_s + ".)      " + thingy.to_s
+	end
 	test.close
 	
 	Dir.glob(File.join("#{tempFile}", "/", "jruby-complete-1.6.3.jar")).each do |file|
 		#puts file
 		#require file
 	end
-	Dir.glob(File.join("#{File.dirname(File.dirname(File.dirname(tempFile)))}", "/D&D 0.5/", "Characte_Generator.rb")).each do |file|
+	Dir.glob(File.join("#{File.dirname(File.dirname(File.dirname(tempFile)))}", "/D&D 0.5/", "Character_Generator.rb")).each do |file|
 		#puts file
 		require file
 	end
-	#generator = CharacterGenerator.new
+	generator = CharacterGenerator.new
 	#generator.generate_specific_level_characters(number_of_characters.to_i, char_level.to_i, num_classes.to_i,)
+	generator.generate_level_range_characters(argList[2].to_i, argList[4].to_i, argList[5].to_i, 1, ["PHB","MM1"], argList[1], argList[0])
 	#return tempList.values.join(";")
 end
